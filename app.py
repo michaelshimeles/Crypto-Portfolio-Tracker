538ed483389dd91f310def5c1252a929a1e2515d
Michael Shimeles <michaelshimeles@MacBook-Pro.phub.net.cable.rogers.com>
2022-06-23 13:21:45 -0400

    Created a database where crypto portfolio & transactions are tracked. Buying is semi working, having some issues with timestamp and total amount paid/sold, I also forgot BUY/SELL

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from cProfile import run 
+from cProfile import run
+from crypt import crypt
 import os
 
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
-from flask_session import Session 
+from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required, usd
+from pycoingecko import CoinGeckoAPI
 
 # Configure application
 app = Flask(__name__)
@@ -26,6 +28,10 @@ Session(app)
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///crypto.db")
 
+# Configure CoinGeckoAPI
+cg = CoinGeckoAPI()
+
+
 @app.after_request
 def after_request(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
@@ -33,10 +39,57 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+
 @app.route("/")
 @login_required
 def index():
     return render_template("index.html")
+
+
+@app.route("/price", methods=["GET", "POST"])
+@login_required
+def price():
+    if request.method == "POST":
+        crypto = request.form.get("crypto").lower()
+
+        # Check if crypto is valid
+        # coinlist = cg.get_coins_list().lower() <- come back to this
+
+        coin_price = cg.get_price(crypto, vs_currencies='usd')
+        final_price = coin_price[crypto]["usd"]
+
+        return render_template("price.html", final_price=final_price)
+
+    else:
+        return render_template("price.html")
+
+
+@app.route("/buy", methods=["GET", "POST"])
+@login_required
+def buy():
+    if request.method == "POST":
+
+        crypto = request.form.get("crypto").lower()
+        amount = request.form.get("amount")
+
+        coin_price = cg.get_price(crypto, vs_currencies='usd')
+        final_price = coin_price[crypto]["usd"]
+
+        if not request.form.get("crypto"):
+            return apology("You must provide a crypto", 400)
+
+        if not request.form.get("amount"):
+            return apology("You must provide an amount", 400)
+
+        # Add transaction to database
+        db.execute("INSERT INTO crypto_txs (user_id, tx_type, crypto, num_of_coins, price_per_coin, total_amount_tx) VALUES (?, ?, ?, ?, ?, ?)", session["user_id"], "buy", crypto, amount, final_price, int(final_price) * amount)
+
+        return redirect("/")
+
+        # currently timestamp is not working and total amount is not working
+    
+    else:
+        return render_template("buy.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -47,17 +100,18 @@ def login():
     if request.method == "POST":
         if not request.form.get("username"):
             return apology("must provide username", 400)
-        
+
         if not request.form.get("password"):
             return apology("must provide password", 400)
 
         # Query database for username
-        userInfo = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        userInfo = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         # Ensure username exists and password is correct
         if len(userInfo) != 1 or not check_password_hash(userInfo[0]["hash"], request.form.get("password")):
             return apology("invalid username and/or password", 400)
-        
+
         # Remember which user has logged in
         session["user_id"] = userInfo[0]["id"]
 
@@ -66,6 +120,7 @@ def login():
 
     else:
         return render_template("login.html")
+
 
 @app.route("/logout")
 @login_required
@@ -83,13 +138,13 @@ def register():
 
         if not request.form.get("firstname"):
             return apology("must provide first name", 400)
-        
+
         elif not request.form.get("lastname"):
             return apology("must provide last name", 400)
 
         elif not request.form.get("username"):
             return apology("must provide username", 400)
-        
+
         elif not request.form.get("password"):
             return apology("must provide password", 400)
 
@@ -97,17 +152,19 @@ def register():
             return apology("Password does not match", 400)
 
         # Query database for username
-        checkUsername = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        checkUsername = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         # Ensure username is not already taken
         if len(checkUsername) != 0:
             return apology("Username already exists", 400)
 
         # Store username and password in database
-        userInfo = db.execute("INSERT INTO users (username, firstname, lastname, hash) VALUES(?, ?, ?, ?)", request.form.get("username"), request.form.get("firstname"), request.form.get("lastname"), generate_password_hash(request.form.get("password")))
+        userInfo = db.execute("INSERT INTO users (username, firstname, lastname, hash) VALUES(?, ?, ?, ?)", request.form.get(
+            "username"), request.form.get("firstname"), request.form.get("lastname"), generate_password_hash(request.form.get("password")))
 
         # Redirect user to home page
         return redirect("/")
-    
+
     else:
         return render_template("register.html")
